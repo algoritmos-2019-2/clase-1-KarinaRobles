f6692ee0ddc5cc12c12b2e968db3e4249effa70d
Karina Robles <karinarobles@ciencias.unam.mx>
2019-02-20 13:04:23 -0600

    Practica1

---
+++ Practica_1/PRACT1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3.7
+
+#TABLA DE VERDAD OPERADOR AND
+
+
+x = [True, False]
+
+y = [True, False]
+
+for i in x:
+    print ( x and y)
+    for i in y:
+        print (x or y)
+
+
+
